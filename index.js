e0c20902d0d71b14304898f97a3c5bd7e0266fea
Esshka <esshka@gmail.com>
2016-02-02 09:05:23 +0300

    server

---
+++ index.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+// var staticPath = path.resolve(__dirname, 'static');
+// console.log(staticPath)
+// app.use(express.static(staticPath));
+app.use('/static', express.static(__dirname + '/static'));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, function() {
+  console.log('listening');
+});
